4af445991fe7a14f2e9927ec9a1ae26b48309ff3
Alexandre Spindola <alexandre.spindola11@gmail.com>
2025-01-13 17:06:10 +0100

    remove outdated book documents and update access token in generateBooks.ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,42 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'static',
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
+  output: "static",
+  integrations: [
+    starlight({
+      title: "Gnosis",
+      defaultLocale: "root",
+      locales: {
+        de: {
+          label: "Deutsch",
+        },
+        en: {
+          label: "English",
+        },
+        root: {
+          label: "Español",
+          lang: "es",
+        },
+        fr: {
+          label: "Français",
+        },
+        it: {
+          label: "Italiano",
+        },
+        nl: {
+          label: "Nederlands",
+        },
+        pt: {
+          label: "Português",
+        },
+      },
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      /* sidebar: [
 				{
 					label: 'Libros',
 					items: [
@@ -19,7 +44,7 @@ export default defineConfig({
 						{ label: 'Educación Fundamental', slug: 'books/example' },
 					],
 				},
-			],
-		}),
-	],
+			], */
+    }),
+  ],
 });
