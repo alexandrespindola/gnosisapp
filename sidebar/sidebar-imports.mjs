ea118ff48d890d9e6d406b21ecd1c195ea23c040
Alexandre Spindola <alexandre.spindola11@gmail.com>
2025-03-17 18:09:55 +0100

    Refactors sidebar translations
    Simplifies the sidebar configuration by importing translations
    from a dedicated file, improving maintainability and readability.
    
    This change reduces redundancy and makes it easier to manage
    translations for various sidebar labels.

---
+++ sidebar/sidebar-imports.mjs
@@ -0,0 +1,4 @@
+export { librosTranslations } from './librosTranslations.mjs';
+export { educacionFundamentalTranslations } from './educacionFundamentalTranslations.mjs';
+export { laGranRebelionTranslations } from './laGranRebelionTranslations.mjs';
+export { tratadoDePsicologiaRevolucionariaTranslations } from './tratadoDePsicologiaRevolucionariaTranslations.mjs';
